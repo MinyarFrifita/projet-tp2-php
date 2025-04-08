908344ad716012b7ef823e14e9b8b28176e40aa2
RayenGuedrii <rayen.guedri@insat.ucar.tn>
2025-04-08 16:42:35 +0100

    Update Student.php

--- projetListStudents/Student.php
+++ projetListStudents/Student.php
@@ -1,19 +1,38 @@
 <?php
-require_once '../Autoloader.php';
+require_once 'Autoloader.php';
 Autoloader::register();
 
 class Student {
+    private static $repository;
+
+    public static function init() {
+        self::$repository = new StudentRepository();
+    }
+
     public static function getAll() {
-        $db = Database::connect();
-        $stmt = $db->query("SELECT * FROM student");
-        return $stmt->fetchAll();
+        return self::$repository->findAll();
     }
 
     public static function getById($id) {
-        $db = Database::connect();
-        $stmt = $db->prepare("SELECT * FROM student WHERE id = ?");
-        $stmt->execute([$id]);
-        return $stmt->fetch();
+        return self::$repository->findById($id);
+    }
+
+    public static function create($data) {
+        return self::$repository->create($data);
+    }
+
+    public static function update($id, $data) {
+        return self::$repository->update($id, $data);
+    }
+
+    public static function delete($id) {
+        return self::$repository->delete($id);
+    }
+
+    public static function findBySection($sectionId) {
+        return self::$repository->findBySection($sectionId);
     }
 }
-?>
+
+// Initialize the repository
+Student::init();
